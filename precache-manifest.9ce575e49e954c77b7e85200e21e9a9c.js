b66d5c486bc4c712f53916ac5696558a1bb3ad43
Suhair Zain <suhair@languagezen.com>
2020-06-23 19:32:45 +0530

    Updates

---
+++ precache-manifest.9ce575e49e954c77b7e85200e21e9a9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "361ebdcf3d0775cce10707d1950f313b",
+    "url": "/azumio-test-design/index.html"
+  },
+  {
+    "revision": "b4eece81a9775ddbd524",
+    "url": "/azumio-test-design/static/css/main.6fb850df.chunk.css"
+  },
+  {
+    "revision": "ff519bd1a1003553fae1",
+    "url": "/azumio-test-design/static/js/2.86f0114a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/azumio-test-design/static/js/2.86f0114a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4eece81a9775ddbd524",
+    "url": "/azumio-test-design/static/js/main.b1d01a9e.chunk.js"
+  },
+  {
+    "revision": "8046329bdb9c5a78611d",
+    "url": "/azumio-test-design/static/js/runtime-main.b495638e.js"
+  }
+]);
